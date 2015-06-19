070ecbfd854eeabf0d1e65b3d13133b2a4df8f9e
Mirko <mirko9662@gmail.com>
2015-06-20 01:52:50 +0200

    Missing comments
    Added new comments

--- server.py
+++ server.py
@@ -3,17 +3,20 @@ import sys
  
 HOST = ''   # Symbolic name, meaning all available interfaces
 PORT = 8888 # Arbitrary non-privileged port
- 
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print ('Socket created')
  
 # Bind socket to local host and port
 try:
     s.bind((HOST, PORT))
+
+# Handle exception socket error
 except socket.error as msg:
     print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
     sys.exit()
-     
+
+# Prompt the following message 
 print ('Socket bind complete')
  
 # Start listening on socket
@@ -26,5 +29,6 @@ while 1:
     # Wait to accept a connection - blocking call
     conn, addr = s.accept()
     print ('New client connected')
-     
+
+# Close the socket
 s.close()
