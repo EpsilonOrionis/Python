003bb74531b566f76d6ea94a63a05e7694b11b15
Mirko <mirko9662@gmail.com>
2015-06-20 01:29:43 +0200

    Test
    Test

---
+++ email.py
@@ -0,0 +1,29 @@
+# Import modules
+import getpass
+import imaplib
+ 
+# Set server address in the server variable
+server = 'imap.googlemail.com'
+
+# Set server port in the port variable
+port = '993'
+
+# Prompt the following message
+print('enter the email address you wish to read the emails from please!')
+
+# Get email address in the username variable
+username = input('email address:')
+ 
+# Create session
+mailbox = imaplib.IMAP4_SSL(server,port)
+
+# Get account password in the password variable
+password = input('enter your password: ')
+
+# Login
+mailbox.login(username,password)
+status,count = mailbox.select('Inbox')
+status,data= mailbox.fetch(count[0],'(UID BODY[TEXT])')
+ 
+# Print data
+print(data[0][1])
