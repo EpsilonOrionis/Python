a23aaf0338122bc13ec2e7281658a6e65906edf8
Mirko <mirko9662@gmail.com>
2015-06-28 01:36:04 +0200

    Other tests

---
+++ first.py
@@ -0,0 +1,15 @@
+# First Python script
+import sys
+import os
+print(sys.platform)
+print(2 ** 100)
+x = 'Spam!'
+# Print x * 8
+print(x * 8)
+a = eval(input())
+b = eval(input())
+c= a+b
+print(c)
+stringa = input()
+print(stringa)
+os.system("PAUSE")
--- server.py
+++ server.py
@@ -1,12 +1,13 @@
+# Import
 import socket
 import sys
- 
+
 HOST = ''   # Symbolic name, meaning all available interfaces
 PORT = 8888 # Arbitrary non-privileged port
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print ('Socket created')
- 
+
 # Bind socket to local host and port
 try:
     s.bind((HOST, PORT))
@@ -16,13 +17,13 @@ except socket.error as msg:
     print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
     sys.exit()
 
-# Prompt the following message 
+# Prompt the following message
 print ('Socket bind complete')
- 
+
 # Start listening on socket
 s.listen(10)
 print ('Socket now listening')
- 
+
 # Now keep talking with the client
 while 1:
 
@@ -32,3 +33,4 @@ while 1:
 
 # Close the socket
 s.close()
+# Fine
